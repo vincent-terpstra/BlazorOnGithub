b1b80c0f104a1db6954f83490da22b363dfb96af
Vincent Terpstra <vdterpst@gmail.com>
2024-01-08 20:41:47 -0500

    Adding a service to handle saving of appstate

---
+++ BlazorOnGithub/Services/IMemoryService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorOnGithub.Services;
+
+public interface IMemoryService
+{
+    public Task<T?> GetItemAsync<T>(string key);
+
+    public Task SetItemAsync<T>(string key, T value);
+}
---
+++ BlazorOnGithub/Services/JSMemoryService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using Microsoft.JSInterop;
+
+namespace BlazorOnGithub.Services;
+
+public class JSMemoryService : IMemoryService
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public JSMemoryService(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public async Task<T?> GetItemAsync<T>(string key)
+    {
+        var value = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", key);
+        if (!string.IsNullOrWhiteSpace(value))
+        {
+            return JsonSerializer.Deserialize<T>(value);
+        }
+
+        return default;
+    }
+
+    public async Task SetItemAsync<T>(string key, T value)
+    {
+        var item = JsonSerializer.Serialize(value);
+        await _jsRuntime.InvokeVoidAsync("localStorage.setItem", key, item);
+    }
+}
