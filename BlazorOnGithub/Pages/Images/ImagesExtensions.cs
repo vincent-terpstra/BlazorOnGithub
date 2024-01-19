fdac7037834418045d8c8304d76c090b51255823
Vincent Terpstra <vdterpst@gmail.com>
2024-01-18 22:04:56 -0500

    Adding a way to upload and view a single image

---
+++ BlazorOnGithub/Pages/Images/ImagesExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorOnGithub.Pages.Images;
+
+public static class ImagesExtensions
+{
+    public static string ToDataUrl(this MemoryStream data, string format)
+    {
+        var span = new Span<byte>(data.GetBuffer()).Slice(0, (int)data.Length);
+        return $"data:{format};base64,{Convert.ToBase64String(span)}";
+    }
+}
